4d81577f53a448ff8b944f49df1bdc93889b53e4
saifeddine ghouma <saif.g@tnt.com.tn>
2019-06-19 17:53:07 +0100

    drenier tets in formik 31

--- src/Components/FormikForm.js
+++ src/Components/FormikForm.js
@@ -1,26 +1,35 @@
 import React,{Component} from 'react'
-import {Formik,Field} from 'formik'
-import Yup from 'yup'
+import {Formik,Field,ErrorMessage, FieldArray} from 'formik'
+import * as  Yup from 'yup'
 class FormikForm extends Component
 {
 	showName=(value)=>
 	{
 		console.log(value)
 	}
+	
 	form=(props)=>
 	{
 		return (
 
 
    <form onSubmit={props.handleSubmit}>
-<label>Name </label> : <Field name="name"/>
+   <label>Name </label> : <Field name="name"/>
+   <br/>
+   <ErrorMessage name='name'/>
    
    <br/>
    
 
 <label>Last Name </label>  :  <Field name="lasteName"/>
+  <br/>
+   <ErrorMessage name='lasteName'/>
+
 <br/>
 <label>Email </label>  :  <Field type="Email" name="email"/>
+  <br/>
+   <ErrorMessage name='email'/>
+
 <br/>
 <label>skills </label>  :
  <Field component="select" name="skills">
@@ -30,6 +39,9 @@ class FormikForm extends Component
 
  </Field>
    <br/>
+   <ErrorMessage name='skills'/>
+   
+   <br/>
    
 
 <label>Active </label>  :  <Field name="active" type="checkbox"/>
@@ -37,7 +49,81 @@ class FormikForm extends Component
 <label>Mobile </label>  :  <Field name="category" value="mobile" type="radio"/>
 <br/><label>Phone </label>  :<Field name="category" value="phone" type="radio"/> 
 <br/>
+   <ErrorMessage name='category'/>
+
 <br/>
+
+<br/>
+<label>Facebook </label>  :  <Field type="text" name="socials.facebook"/>
+  <br/>
+   <ErrorMessage name='socials.facebook'/>
+
+<br/>
+<br/>
+<label>Twitter </label>  :  <Field type="text" name="socials.twitter"/>
+  <br/>
+   <ErrorMessage name='socials.twitter'/>
+
+<br/>
+<br/>
+<label>Instagram </label>  :  <Field type="text" name="socials.instagram"/>
+  <br/>
+   <ErrorMessage name='socials.instagram'/>
+
+<br/>
+<FieldArray
+name="friends"
+render={
+   arrayHelper=>(
+
+   	 <div>
+   	   {props.values.friends.map((item, index)=>
+
+			<div key={index}>
+
+			   <Field name={`friends.${index}`} />
+			           <button type="button" onClick={()=>arrayHelper.remove(index)}>Done!!</button>
+			
+
+   <ErrorMessage name={`friends.${index}`} /><br />
+
+
+
+			</div>
+			        
+
+
+
+   	   	)}
+
+
+			           <button type="button" onClick={()=>arrayHelper.push()}>add!!</button>
+
+
+
+   	 </div>
+
+
+
+
+
+
+
+   	)
+
+}
+
+
+
+/>
+
+
+
+
+
+
+
+
    <button type="submit">Send</button>
 
 
@@ -50,15 +136,72 @@ class FormikForm extends Component
 
 			)
 	}
+
+schema=()=>
+
+	{
+		const schema = Yup.object().shape({
+
+			name: Yup.string().required(),
+
+			lasteName: Yup.string().required(),
+			email: Yup.string().required(),
+			category: Yup.string().required(),
+			skills: Yup.string().required(),
+			socials:Yup.object().shape({
+                facebook: Yup.string().required('facebook  is required'),
+                twitter :Yup.string().required('twitter is required'),
+			instagram: Yup.string().required('instagram is required'),
+
+			
+
+			}),
+
+	 friends: Yup.array().of(
+        Yup.string().required('Required '),
+      )
+
+			
+		})
+		return schema
+		
+	}
+
+
+
+
 	render()
 	{
 		return (
 <Formik
-initialValues={{name:"",lasteName:'',email:'',skills:'',active:true,category:''}}
+initialValues={{
+	name:"",
+	lasteName:'',
+	email:'',
+	skills:'',
+	active:true,
+	category:'',
+    socials:{
+    	facebook:'',
+    	twitter:'',
+    	instagram:''
+    },
+    friends:['saif','bessem','amir'],
+    phoenNumber:[
+    {
+    	number:'50908869',
+    	extension:'orange'
+    },
+    {
+    	number:'20020442',
+    	extension:'oredoo'
+    }
+    ]
+}}
 onSubmit={this.showName}
 
 render={this.form}
-ValidationSchema={this.schema}
+validationSchema={this.schema()}
 />
 )
 	}
